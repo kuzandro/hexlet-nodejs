66b2a478de3ef6d4495488ed8145135619521784
kuzandro <kuzandro@gmail.com>
2019-02-08 23:02:07 +0300

    old task

--- problems.js
+++ problems.js
@@ -1,3 +1,12 @@
+//Константы
+const pi = 3.14; // eslint-disable-line
+
+// BEGIN (write your solution here)
+const weight = 80
+const c = 300000000
+const energy = weight * (c ** 2)
+// END
+
 //Итеративный процесс - наименьший делитель
 
 const smallestDivisor = (num) => {
